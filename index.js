960b8e96f8182eb5d4860577fa36981fc851cd0f
baciu.alex <mircea.alexandru.baciu@students.beaminstitute.org>
2023-07-18 16:07:23 +0200

    made changes

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
+import cors from "cors";
 import messages from "./db.js";
 const app = express();
 app.use(express.static("public"));
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
